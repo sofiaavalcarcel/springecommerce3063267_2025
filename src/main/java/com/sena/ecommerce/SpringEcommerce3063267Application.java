28cd1b8b4056e7fafeae1254873744268cfe351e
sofiavalk <esofiavalcarcel73@gmail.com>
2025-08-13 14:53:34 -0500

    v0.2_configurarAplication.propierties_sofiavalk_13082025

--- src/main/java/com/sena/ecommerce/SpringEcommerce3063267Application.java
+++ src/main/java/com/sena/ecommerce/SpringEcommerce3063267Application.java
@@ -2,9 +2,9 @@ package com.sena.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude=DataSourceAutoConfiguration.class)
+
+@SpringBootApplication
 public class SpringEcommerce3063267Application {
 
 	public static void main(String[] args) {
